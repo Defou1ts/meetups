44c9feda9fdfa565203aef31fce092bba647ed88
Defou1ts <pasnkevikn@gmail.com>
2023-09-05 08:13:32 +0300

    feat: created base users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 
 @Module({
 	imports: [
@@ -13,6 +14,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
 			models: [],
 			autoLoadModels: true,
 		}),
+		UsersModule,
 	],
 })
 export class AppModule {}
